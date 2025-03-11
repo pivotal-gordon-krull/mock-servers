01cb9e746519ab74014f35c77e459e3f79d7e9f8
Gordon Krull <gordon@bekindlabs.com>
2025-03-11 17:02:34 +0900

    spring

---
+++ spring/src/main/kotlin/com/example/todo/controller/TodoController.kt
@@ -0,0 +1,36 @@
+package com.example.todo.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+
+@RestController
+@RequestMapping("/todos")
+class TodoController {
+    private val todos: MutableMap<UUID, Todo> = mutableMapOf()
+
+    @GetMapping
+    fun getTodos(): List<Todo> {
+        return todos.values.toList()
+    }
+
+    @GetMapping("/{id}")
+    fun getTodo(@PathVariable id: UUID): Todo {
+        return todos[id]!!
+    }
+
+    @PostMapping
+    fun createTodo(@RequestBody request: CreateTodoRequest): Todo {
+        val todo = Todo(UUID.randomUUID(), request.title)
+        todos[todo.id] = todo
+        return todo
+    }
+}
+
+data class CreateTodoRequest(val title: String)
+data class Todo(val id: UUID, val title: String)
