6612bd1fbaa4377a77e96c5e2f12edc04c6ebf01
Gordon Krull <gordon@bekindlabs.com>
2025-03-12 18:05:00 +0900

    spring swagger.json

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.8.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- spring/src/main/kotlin/com/example/todo/controller/TodoController.kt
+++ spring/src/main/kotlin/com/example/todo/controller/TodoController.kt
@@ -1,5 +1,6 @@
 package com.example.todo.controller
 
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -29,6 +30,11 @@ class TodoController {
         val todo = Todo(UUID.randomUUID(), request.title)
         todos[todo.id] = todo
         return todo
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteTodo(@PathVariable id: UUID) {
+        todos.remove(id)
     }
 }
 
